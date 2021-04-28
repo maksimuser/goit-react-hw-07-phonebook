8d0ae2caf9496dc67529bb0fbf62503f4fbf056f
Dema Maksim <maksimdema@gmail.com>
2021-04-28 10:04:51 +0300

    Updates

--- static/js/main.3c28f9fb.chunk.js
+++
@@ -1,2 +0,0 @@
-(this["webpackJsonpgoit-react-hw-07-phonebook"]=this["webpackJsonpgoit-react-hw-07-phonebook"]||[]).push([[0],{10:function(e,t,o){"use strict";o.r(t);var n=o(1),c=o.n(n),r=o(3),i=o.n(r),h=o(0),s=function(){return Object(h.jsx)("div",{children:"HW-07-phonebook"})};o(9);i.a.render(Object(h.jsx)(c.a.StrictMode,{children:Object(h.jsx)(s,{})}),document.getElementById("root"))}},[[10,1,2]]]);
-//# sourceMappingURL=main.3c28f9fb.chunk.js.map
